d55da6397d33e185f47d01051f5675ca25c9b642
Matheo Gerard <mgerard@fu-r4-p5.s19.be>
2026-07-03 11:35:46 +0200

    add exos in folder 4

--- ex4/decorator_mastery.py
+++ ex4/decorator_mastery.py
@@ -24,6 +24,56 @@ def spell_timer(func: Callable) -> Callable:
     return wrapper
 
 
+def power_validator(min_power: int) -> Callable:
+    if not isinstance(min_power, int):
+        raise TypeError("min_power must be an int")
+
+    def deco(func: Callable):
+        @wraps(func)
+        def wrapper(*args, **kwargs):
+            power: int = args[0]
+            if power < min_power:
+                return "Insufficient power for this spell"
+            else:
+                return func(*args, **kwargs)
+
+        return wrapper
+
+    return deco
+
+
+def test(n: int) -> str:
+    if n != 2:
+        raise ValueError("RATER")
+    else:
+        return f"Pass in {n} try"
+
+
+def retry_spell(max_attempts: int) -> Callable:
+    def deco(func: Callable) -> Callable:
+        @wraps(func)
+        def wrapper(*args, **kwargs) -> str:
+            for x in range(0, max_attempts):
+                try:
+                    return func(*args, **kwargs)
+                except Exception:
+                    print(
+                        f"Spell failed, retrying... (attempt {x + 1}/{max_attempts})"
+                    )
+            return f"Spell casting failed after {max_attempts} attempts"
+
+        return wrapper
+
+    return deco
+
+
+class MageGuild:
+    @staticmethod
+    def validate_mage_name(name) -> bool:
+        # TODO: Check if the conditions for the name is respected
+        pass
+
+
 if __name__ == "__main__":
     print("Testing spell timer...")
     try:
